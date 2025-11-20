6841d4afcc19fdbd0c9ced8c1267a88e20cb8b4d
Devesh <rawal2003devesh@gmail.com>
2025-11-20 11:17:13 +0530

    finaly solved the session login- cookies work on same server localhost only

--- student-management/src/main/java/com/example/student_management/config/CorsConfig.java
+++ student-management/src/main/java/com/example/student_management/config/CorsConfig.java
@@ -13,8 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500/", "http://localhost:5500")
-                        .allowedMethods("*")
+                        .allowedOrigins("http://192.168.1.10:5500","http://127.0.0.1:5500", "http://localhost:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Set-Cookie")
                         .allowCredentials(true);
             }
         };
--- student-management/src/main/java/com/example/student_management/controller/UserController.java
+++ student-management/src/main/java/com/example/student_management/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -54,6 +55,7 @@ public class UserController {
 
     @GetMapping("/check-login")
     public ResponseEntity<?> checkLogin(HttpSession session) {
+
         if (session.getAttribute("userId") != null) {
             return ResponseEntity.ok("LoggedIn");
         } else {
